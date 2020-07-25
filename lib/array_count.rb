ced846cff945b7eb1b87fbb45a786bb8238700c9
Seohee  Im <seoheeim@vt.edu>
2020-07-25 23:30:50 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,10 +1,8 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
   string = 0 
-  while [array.count.is_a?(String) do
-    string += 1 
-  ]
-  
+  while array.count.is_a?(String) do
+    string += 1
 end
 
 def count_empty_strings(array)
